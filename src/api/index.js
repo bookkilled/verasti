324ba91f4d5ebb89870d721060beba32827f6ebf
bookkilled <bookkilled@gmail.com>
2017-10-25 00:42:13 +0800

    完善兼容性9“

--- src/api/index.js
+++ src/api/index.js
@@ -3,18 +3,18 @@
  */
 import reqwest from 'reqwest'
 
-const setTimeout = 20000
-const DEV_ENV = process.env.DEV_ENV || ''
+var setTimeout = 20000
+var DEV_ENV = process.env.DEV_ENV || ''
 
-// const domain = (DEV_ENV === 'production')
+// var domain = (DEV_ENV === 'production')
 // 	? `${location.protocol}//admin.verasti.com`
 //   : `${location.protocol}//116.62.165.234`
-const domain = location.protocol+'//116.62.165.234';
-// const domain = ( DEV_ENV.replace(/\s/g,"") === 'production')
+var domain = location.protocol+'//116.62.165.234';
+// var domain = ( DEV_ENV.replace(/\s/g,"") === 'production')
 // 	? `http://devwww.ineleadbank.com.cn`
 //   : `http://devwww.ineleadbank.com.cn`
-// const Hmethod =  (DEV_ENV.replace(/\s/g,"") === 'production') ? `POST` : `GET`
-const Hmethod = 'GET'
+// var Hmethod =  (DEV_ENV.replace(/\s/g,"") === 'production') ? `POST` : `GET`
+var Hmethod = 'GET'
 
 // 获取banner地址
 export function getBanner() {
